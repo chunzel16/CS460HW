f4a136c45d304e0c25bd7a84844627bbcc31b913
Chunze-Liu <32441897+15042295553@users.noreply.github.com>
2018-10-15 14:13:48 +0800

    add C file

---
+++ HW3/BFS/BFS/LinkedQueue.cs
@@ -0,0 +1,83 @@
+﻿
+
+/// <summary>
+/// A Singly Linked FIFO Queue.  
+/// From Dale, Joyce and Weems "Object-Oriented Data Structures Using Java"
+/// Modified for CS 460 HW3
+/// 
+/// See QueueInterface.java for documentation
+/// </summary>
+
+public class LinkedQueue<T> : IQueueInterface<T>
+{
+	private Node<T> front;
+	private Node<T> rear;
+
+	public LinkedQueue()
+	{
+		front = null;
+		rear = null;
+	}
+
+	public virtual T Push(T element)
+	{
+		if (element == null)
+		{
+			throw new System.NullReferenceException();
+		}
+
+		if (Empty)
+		{
+			Node<T> tmp = new Node<T>(element, null);
+			rear = front = tmp;
+		}
+		else
+		{
+			// General case
+			Node<T> tmp = new Node<T>(element, null);
+			rear.next = tmp;
+			rear = tmp;
+		}
+		return element;
+	}
+
+	public virtual T Pop()
+	{
+		T tmp = default(T);
+		if (Empty)
+		{
+			throw new QueueUnderflowException("The queue was empty when pop was invoked.");
+		}
+		else if (front == rear)
+		{ // one item in queue
+			tmp = front.data;
+			front = null;
+			rear = null;
+		}
+		else
+		{
+			// General case
+			tmp = front.data;
+			front = front.next;
+		}
+
+		return tmp;
+	}
+
+	public virtual bool Empty
+	{
+		get
+		{
+			if (front == null && rear == null)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+
+}
+
