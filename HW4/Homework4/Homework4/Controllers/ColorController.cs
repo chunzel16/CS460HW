62d984f1f97805f99abb8899d2782d7cd9036b62
Chunze-Liu <32441897+15042295553@users.noreply.github.com>
2018-10-22 12:52:08 +0800

    add MVC file and screenshot

---
+++ HW4/Homework4/Homework4/Controllers/ColorController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework4.Controllers
+{
+    public class ColorController : Controller
+    {
+        // GET: Converter
+        [HttpGet]
+        public ActionResult MixColor()
+        {
+            ViewBag.show = false;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult MixColor(string FirstColor, string SecondColor)
+        {
+            //Get the inputs
+            FirstColor = Request.Form["FirstColor"];
+            SecondColor = Request.Form["SecondColor"];
+
+            //make sure the color input are not null
+            if (FirstColor != null && SecondColor != null)
+            {
+                ViewBag.show = true;
+                Color FirstInput = ColorTranslator.FromHtml(FirstColor);
+                Color SecondInput = ColorTranslator.FromHtml(SecondColor);
+
+                //Calculate new color
+                Color MixedColor = new Color();
+
+                //Initialize RGB
+                int red, green, blue = 0;
+
+                //Make sure the value <= 255
+                if (FirstInput.R + SecondInput.R > 255)
+                {
+                    red = 255;
+                }
+                else
+                {
+                    red = FirstInput.R + SecondInput.R;
+                }
+
+                if (FirstInput.G + SecondInput.G > 255)
+                {
+                    green = 255;
+                }
+                else
+                {
+                    green = FirstInput.G + SecondInput.G;
+                }
+
+                if (FirstInput.B + SecondInput.B > 255)
+                {
+                    blue = 255;
+                }
+                else
+                {
+                    blue = FirstInput.B + SecondInput.B;
+                }
+
+                //define MixedColor
+                MixedColor = Color.FromArgb(255, red, green, blue);
+                //define the result to ResultColor
+                string ResultColor = ColorTranslator.ToHtml(MixedColor);
+
+                //Return all three color 
+                ViewBag.color1 = "background:" + FirstColor;
+                ViewBag.color2 = "background:" + SecondColor;
+                ViewBag.newcolor = "background:" + ResultColor;
+            }
+            return View();
+        }
+    }
+}
---
+++ HW4/Homework4/Homework4/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Converter()
+        {
+            //Input the miles
+            string miles = Request.QueryString["mileInput"];
+            //Input the metric
+            string metric = Request.QueryString["units"];
+            //Initializes input
+            double newmile = 0;
+            //Make sure the input is not null
+            if (miles != null)
+            {
+                if (miles != "")
+                {
+                    newmile = Convert.ToDouble(miles);
+                }
+                else
+                {
+                    return View();
+                }
+
+                //Make sure what units user choose
+                if (metric == "millimeters")
+                {
+                    newmile = newmile * 1609344.0;
+                }
+                else if (metric == "centimeters")
+                {
+                    newmile = newmile * 160934.4;
+                }
+                else if (metric == "meters")
+                {
+                    newmile = newmile * 1609.344;
+                }
+                else if (metric == "kilometers")
+                {
+                    newmile = newmile * 1.609344;
+                }
+
+                //Calculation result number
+                string outputNumber = newmile.ToString();
+
+                //Final output
+                ViewBag.message = miles + " miles is equal to " + outputNumber + " " + metric;
+            }
+            return View();
+
+
+
+        }
+    }
+}
