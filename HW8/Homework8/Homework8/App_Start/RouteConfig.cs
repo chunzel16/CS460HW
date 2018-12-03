d1c2c1ba678aa9eb9a6fd18b102b06a4837760f2
Chunze-Liu <32441897+15042295553@users.noreply.github.com>
2018-12-03 13:40:02 +0800

    azure password

--- HW8/Homework8/Homework8/App_Start/RouteConfig.cs
+++ HW8/Homework8/Homework8/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Homework8
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Auction", action = "Home", id = UrlParameter.Optional }
             );
         }
     }
